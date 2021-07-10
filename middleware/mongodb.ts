72fa1542ff58bb775e3a7474eaf817785fac2a8a
wmoralesdev <walterrafael26@gmail.com>
2021-07-10 02:18:08 -0600

    Fixed types in mongodb

--- middleware/mongodb.ts
+++ middleware/mongodb.ts
@@ -1,7 +1,7 @@
 /* This is a database connection function*/
 import mongoose from 'mongoose'
 
-const connectDB = handler => async (req, res) => {
+const connectDB = (handler: any) => async (req: any, res: any) => {
     if (mongoose.connections[0].readyState) {
         // Use current db connection
         return handler(req, res);
