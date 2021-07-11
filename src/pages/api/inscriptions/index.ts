ee2470415a749edded72673573beb2b26fd1224d
wmoralesdev <walterrafael26@gmail.com>
2021-07-11 13:40:29 -0600

    Better visualization for inscriptions lookup

--- src/pages/api/inscriptions/index.ts
+++ src/pages/api/inscriptions/index.ts
@@ -32,9 +32,26 @@ const handler = nc<IInscriptionRequest, NextApiResponse>()
     })
     .get(async (req, res) => {
         try {
-            var inscriptions = await Inscription.find()
+            var inscriptions =  [
+                await Inscription.find({ courseSlug: 'dotnet-api' }, { fullname: true, email: true, phone: true }),
+                await Inscription.find({ courseSlug: 'advanced-cpp' }, { fullname: true, email: true, phone: true }),
+                await Inscription.find({ courseSlug: 'intro-nextjs' }, { fullname: true, email: true, phone: true }),
+            ]
 
-            return res.status(200).json({ data: inscriptions, message: 'success' })
+            return res.status(200).json({ data: {
+                dotnet: {
+                    count: inscriptions[0].length,
+                    items: inscriptions[0],
+                },
+                'advanced-cpp': {
+                    count: inscriptions[1].length,
+                    items: inscriptions[1],
+                },
+                'intro-nextjs': {
+                    count: inscriptions[2].length,
+                    items: inscriptions[2],
+                },
+            }})
         }
         catch(err) {
             return res.status(400).json({ data: null, message: 'error' })
