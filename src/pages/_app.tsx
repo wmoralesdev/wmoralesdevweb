dc159e2d99f3eb43d68f4185e28823f56ab5969c
wmoralesdev <walterrafael26@gmail.com>
2021-07-11 22:29:10 -0600

    Added next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { DefaultSeo } from 'next-seo'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+          openGraph={{
+            images: [{
+              url: 'https://cursobacks3.s3.amazonaws.com/meta.png',
+              width: 1200,
+              height: 627,
+              alt: 'banner'
+            }]
+          }}
+        />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
